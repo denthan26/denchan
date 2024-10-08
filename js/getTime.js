984055d86de9fe01e8263d5b09f000e90fa749ed
denthen26 <2834796821@qq.com>
2024-10-08 08:28:08 +0800

    实现主题模式切换，白天黑夜

--- js/getTime.js
+++ js/getTime.js
@@ -4,9 +4,11 @@ const hTime = document.getElementById('header_time');
 function updateTime() {
     let now = new Date();
     console.log(now);
+    now = now.toString().split('GMT')[0];
     hTime.innerHTML = now;
 }
 
 updateTime();
 
+// 每秒更新一次
 setInterval(updateTime, 1000);
