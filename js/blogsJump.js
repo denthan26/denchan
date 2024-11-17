a9eae2e33e9e14923393619d082a5e02175a2025
denthan26 <2834796821@qq.com>
2024-11-17 09:26:27 +0800

    Wins上使用ffmpeg录屏并设置编码格式

--- js/blogsJump.js
+++ js/blogsJump.js
@@ -1,4 +1,8 @@
 const win11 = document.getElementById("win11");
+const ffmpeg = document.getElementById("ffmpeg");
 win11.addEventListener("click", () => {
     window.location.href = '/blogspage/Win11重装问题解决.html';
+})
+ffmpeg.addEventListener("click", () => {
+    window.location.href = '/blogspage/ffmpeg相关命令.html';
 })
